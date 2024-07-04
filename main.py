a06bb817bddf4d0ef693b135742fd2ecb50f8bce
fotftor <116300006+fotftor@users.noreply.github.com>
2024-07-04 11:33:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,53 +1,68 @@
 import asyncio
-import config
 import aiogram
 import logging
-from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
-from aiogram import Router, F
-from aiogram.filters import CommandStart
-from aiogram.types import KeyboardButton, ReplyKeyboardMarkup,Message
-from aiogram import html
-import string,random
-from openai import OpenAI
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import Message
 import openai
+import os
+from dotenv import load_dotenv
 
 
+load_dotenv('api.env')
 logging.basicConfig(level=logging.INFO)
 dp = Dispatcher()
-bot = Bot(config.token)
+bot = Bot(os.getenv("TOKEN"))
 messages = []
+image=[]
 
 class tg:
 
     @dp.message(Command("start"))
-    async def cmd_start(message: types.Message):
-        await message.answer(
-            f"Hello")
-        keyboard = ReplyKeyboardMarkup(
-            resize_keyboard=True,
-            one_time_keyboard=True,
-            input_field_placeholder="Воспользуйтесь меню:")
-        button_1 = KeyboardButton("генерация пароля")
-        keyboard.add(button_1)
+    async def cmd_start(self,message: types.Message):
+        await message.answer(f"Hello")
+
 
     @dp.message()
-    async def gen_gpt(message: types.Message):
+    async def gen_gpt(self,message: types.Message):
         global messages
         messages.append({"role": 'user', "content": message.text})
         try:
+            model = "gpt-4o-2024-05-13"
             client = openai.OpenAI(
-                api_key=config.api_den,
+                api_key=os.getenv("DOMAIN"),
                 base_url="https://api.proxyapi.ru/openai/v1")
-            chat_completion = client.chat.completions.create(model="gpt-4o-2024-05-13",
-                                                             messages=[{"role": "user", "content": message.text}])
+            chat_completion = client.chat.completions.create(model=model,messages=[{"role": "user", "content": message.text}])
             response = chat_completion.choices[0].message.content
             await message.answer(response)
             messages.append({"role": 'assistant', "content": response})
         except Exception as e:
             logging.error(f"Error: {e}")
             await message.answer("Error occurred. Please try again later.")
-    # Запуск процесса поллинга новых апдейтов
+
+    @dp.message(Command("image"))
+    async def image_handler(self,message: Message):
+        global image
+        image.append({"role": 'user', "content": message.text})
+        try:
+            model = "dall-e-3"
+            client = openai.OpenAI(
+                api_key=os.getenv("DOMAIN"),
+                base_url="https://api.proxyapi.ru/openai/v1")
+
+            chat_completion1 = client.chat.completions.create(model=model,messages=[{"role": "user", "content": message.text}])
+            response1 = chat_completion1.images.generate(
+                model="dall-e-3",
+                prompt=message.text[4:].split(maxsplit=1),
+                n=1,
+                size="1024x1024",
+            )
+            await message.answer_photo(response1.data.url)
+        except Exception as e:
+            logging.error(f"Error: {e}")
+            await message.answer("Error occurred. Please try again later.")
+
+
     async def main(self):
         await dp.start_polling(bot)
 
@@ -56,3 +71,4 @@ class tg:
 if __name__ == "__main__":
     tg_bot=tg()
     asyncio.run(tg_bot.main())
+
